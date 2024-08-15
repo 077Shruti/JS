1f731ddd336b2e4927e23539cbebd15cc9a15519
Shruti Chauhan <117182462+077Shruti@users.noreply.github.com>
2024-08-15 09:23:19 +0000

    conversion of datatypes

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,18 @@
+let score="true"
+//console.log(typeof score)
+//console.log(typeof (score))
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+//1=>true; 0=>false;
+//""=>false
+//"shruti"=>true
+let sum=90
+let stringSum=String(sum)
+console.log(typeof stringSum)
