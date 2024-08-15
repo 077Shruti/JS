d6fe547e305bc3503eaddf1c4e89f73a982789dd
Shruti Chauhan <117182462+077Shruti@users.noreply.github.com>
2024-08-15 09:07:37 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version(no need to write but it looks nice)
+//alert(3+3):we are using node js not browser
+//code readibility should be hight
+console.log(3+3)
+let name="shruti"
+let age=20
+let isLoggedIn=true;
+//number=>rang(2^53)
+//bigint
+//string=> ""
+//boolean=>true/false
+//null=>stand alone value(type)
+//undefined=>when no value defined to a variable
+//symbol=>use mostly in react to define the content(unique)
+
+
+//OBJECT
+console.log(typeof"Shrut")
+console.log(typeof null); ///object
+console.log(typeof undefined)//undefined
