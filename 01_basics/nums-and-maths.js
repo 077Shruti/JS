933f3aaa2b7efe7b3e9ed75ffbd832850de03431
Shruti Chauhan <117182462+077Shruti@users.noreply.github.com>
2024-08-18 05:34:02 +0000

    Nums-and-maths

---
+++ 01_basics/nums-and-maths.js
@@ -0,0 +1,21 @@
+//********Nums************
+const score=400
+console.log(score)
+const balance=new Number(100)
+console.log(balance);
+console.log((balance.toString().length));
+console.log(balance.toFixed(2))
+const other=123.89
+console.log(other.toPrecision(3))//round of the digits from starting
+const b=1000000
+console.log(b.toLocaleString('en-IN'))//1,000,000 in india system 
+//************************Maths*****************
+console.log(Math)
+console.log(Math.abs(-2))
+console.log(Math.round(4.5))
+console.log(Math.ceil(67.5))
+console.log(Math.pow(2,3))
+console.log(Math.floor(Math.random()*10)+1)
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)//want a random value in min range formula
