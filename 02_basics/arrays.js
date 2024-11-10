689c86a94186a8b83bef3800335ff564944b69e1
Shruti Chauhan <117182462+077Shruti@users.noreply.github.com>
2024-11-10 06:13:06 +0000

    01_arrays done

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -10,4 +10,15 @@ console.log(arr);//[ 9, 2, 3, 'Shruti', 'Muskiii' ]
 
 console.log(arr.includes(3));//true
 console.log(arr.indexOf(2));//1
+const narr=arr.join();//join and convert it into string
+console.log(narr);//9,2,3,Shruti,Muskiii
 
+//slice or splice
+console.log('A',arr);
+const narr2=arr.slice(1,3);
+console.log(narr2);
+console.log('B',arr);//B [ 9, 2, 3, 'Shruti', 'Muskiii' ]
+const narr3=arr.splice(1,3);
+console.log('C',arr);//C [ 9, 'Muskiii' ]
+console.log(narr3);//[ 2, 3, 'Shruti' ]
+//splice change the original array by remoing the range that was mention it from original array 
